366b60ef12c774b522595555a0a91f46690b7873
ANDRE LUIZ DOS SANTOS CESARIO OLIVEIRA <andre.lscoliveira@senacsp.edu.br>
2026-04-01 16:30:09 -0300

    adcionando Js

---
+++ CadastroDeAlunos/js/index.js
@@ -0,0 +1,25 @@
+// função para buscar o endereço automaticamente
+//  via API dos correios    
+
+async function buscarEndereco()
+{
+    const cep = document.getElementById("cep").value.replace(/\D/g, '');
+
+    if(cep !== ""){
+
+        const expressaovalidacao = /^[0-9] {8}$/;
+        if (expressaovalidacao.test(cep))
+        {
+            const resposta = await fetch(`https://viacep.com/ws/${cep}/json`)
+            const dados = await resposta.json();
+            if(!dados.erro)
+            {
+                document.getElementById('rua').value = dados.logradouro;
+                document.getElementById('bairro').value = dados.bairro;
+                document.getElementById('cidade').value = dados.localidade;
+                
+
+            }
+        }
+    }
+}
